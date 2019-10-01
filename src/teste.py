f6c829d1ddc37ce55d841be91a50a5435bb70e43
alexandre-queiroz <55054061+alexandre-queiroz@users.noreply.github.com>
2019-09-30 21:24:17 -0300

    Update teste.py

--- src/teste.py
+++ src/teste.py
@@ -1,1 +1,30 @@
+def ehPrimo(x):
+    cont = 1
+    if x == 1:
+        return True
+    else:
+        for y in range (2,x):
+            if x % y == 0:
+                cont = cont + 1
+        if cont < 2:
+            return True
+        else:
+            return False
 
+
+def contaPrimos(num):
+    lista = []
+    i = 1
+    cont = 0
+    while num > cont:
+        if ehPrimo(i):
+            cont = cont+1
+            lista.append(i)
+            i = i+1
+        else:
+            i = i+1
+    return lista
+
+n = contaPrimos(12)
+
+print(n)
